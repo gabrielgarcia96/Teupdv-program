492492003a2e87b0c924cd3266c0145382e2f26f
Gabriel Garcia <117041401+gabrielgarcia96@users.noreply.github.com>
2024-09-09 17:37:53 -0300

    teste

--- teupdv/src/main/java/com/garciasolutions/teupdv/models/view/LoginView.java
+++ teupdv/src/main/java/com/garciasolutions/teupdv/models/view/LoginView.java
@@ -90,7 +90,7 @@ public class LoginView extends Application {
         scene.getStylesheets().add(getClass().getResource("/loginstyle.css").toExternalForm());
 
         stage.setScene(scene);
-        stage.setTitle("Login ");
+        stage.setTitle("Login");
         stage.show();
     }
 
